6eb487b09b7ab1915124b3c01580b91a935e29ee
Alan Barbe <alanbarbecabj@gmail.com>
2024-09-30 22:09:45 -0300

    upd

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,9 +24,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="AlanB"
         options={{
-          title: 'Explore',
+          title: 'AlanB',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
